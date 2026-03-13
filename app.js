c162b5a255dc6a74510499560462bc74745fbcde
dhirajhack6 <159863667+dhirajhack6@users.noreply.github.com>
2026-03-13 17:29:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,122 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const { send } = require("process");
+const wrapAsync = require("./utils/wrapAsync");
+const { listingSchema } = require("./schema.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("Hi I am root");
+});
+
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else
+    next();
+};
+
+
+// Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find();
+  res.render("listings/index.ejs", { allListings });
+});
+
+//New route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  id = id.trim();
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// Create Route
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
+
+//Edit route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+validateListing,
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+// Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
+app.use((req, res, next) => {
+  next(new Error("Page Not Found"));
+});
+
+app.use((req, res, next) => {
+  const err = new Error("Page not found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+
+  //   res.status(status).send(message);
+  res.status(status).render("error.ejs", { message });
+});
+
+app.listen(8080, () => {
+  console.log("server is listening on port 8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+
+  image: {
+    type: String,
+    default:
+      "https://i.pinimg.com/1200x/60/48/03/604803ad0776fb5e4693c6f869327fe9.jpg",
+    set: (v) =>
+      v === ""
+        ? "https://i.pinimg.com/1200x/60/48/03/604803ad0776fb5e4693c6f869327fe9.jpg"
+        : v,
+  },
+  
+  
+  price: Number,
+  location: String,
+  country: String,
+})
+
+const listing = mongoose.model("listing", listingSchema);
+module.exports = listing;
